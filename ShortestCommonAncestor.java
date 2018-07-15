83a77d89fe7d8f24ddc696f2a6d580a2c69fbeb5
ElaineSchutte <36045471+ElaineSchutte@users.noreply.github.com>
2018-07-15 17:54:57 -0400

    Add files via upload

---
+++ ShortestCommonAncestor.java
@@ -0,0 +1,20 @@
+public class ShortestCommonAncestor {
+
+  public ShortestCommonAncestor(Digraph G){
+  }
+
+  public int length(int v, int w){
+  }
+     
+  public int ancestor(int v, int w){
+  }
+
+  public int length(Iterable<Integer> subsetA, Iterable<Integer> subsetB){
+  }
+
+  public int ancestor(Iterable<Integer> subsetA, Iterable<Integer> subsetB){
+  }
+
+  public static void main(String[] args){
+  }
+}
---
+++ WordNet.java
@@ -0,0 +1,29 @@
+import edu.princeton.cs.algs4.In;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+
+  
+public class WordNet {
+  
+  public WordNet(String synsets, String hypernyms){ 
+    Scanner synsets = new Scanner(synsets);
+    
+  }
+  
+  public Iterable<String> nouns(){
+    Iterator itr = nouns.iterator
+  }
+  
+  public boolean isNoun(String word){
+  }
+  
+  public String sca(String noun1, String noun2){
+  }
+  
+  public int distance(String noun1, String noun2){
+  }
+  
+  public static void main(String[] args){
+  }
+}
