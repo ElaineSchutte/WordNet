53b82c9c82207199855324969d625af1080b4c67
aepayne3 <aepayne3@crimson.ua.edu>
2018-07-23 12:17:57 -0400

    Compiles cleanly

--- Outcast.java
+++ Outcast.java
@@ -1,27 +1,27 @@
 public class Outcast {
-	private WordNet wn;
+ private WordNet wn;
   
   public Outcast(WordNet wordnet){
-	  wn=wordnet;
+   wn=wordnet;
   }
   
   public String outcast(String[] nouns){
-	  int[nouns.size] distance =
-	  for (int i=0; i<nouns.size; i++) {
-		distance[i]=0;
-		}
-	  
-	  int max=-1, maxIndex=-1;
-	  for (i=0; i<nouns.size; i++) {
-		  for (int j=0; j<nouns.size; j++) {
-			  distance[i]+=wn.distance(nouns[i],nouns[j]);
-			}
-			if (distance[i]> max) {
-				max=distance[i];
-			maxIndex[i];
-			}
-		}
-		return nouns[maxIndex];
+   int distance[] = new int[nouns.length]; 
+   for (int i=0; i<nouns.length; i++) {
+  distance[i]=0;
+  }
+   
+   int max=-1, maxIndex=-1;
+   for (int i=0; i<nouns.length; i++) {
+    for (int j=0; j<nouns.length; j++) {
+     distance[i]+=wn.distance(nouns[i],nouns[j]);
+   }
+   if (distance[i]> max) {
+    max=distance[i];
+    maxIndex = i;
+   }
+  }
+  return nouns[maxIndex];
   }
   
   public static void main(String[] args){
